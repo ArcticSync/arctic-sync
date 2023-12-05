bea400a746a05dfa18bb6bd102d33c72fe9febcb
lucifer0x17 <ayush.aa347@gmail.com>
2023-12-05 22:17:33 +0530

    feat: deployed on mainnet

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -1,20 +1,20 @@
 import { createContract } from 'arweavekit/contract'
 import fs from "fs"
-import { ENV } from "../env.js"
 
-const w = JSON.parse(fs.readFileSync("./contracts/wallet.json", "utf8"))
-const src = fs.readFileSync("./contracts/swiftify.js", "utf8")
-const istate = fs.readFileSync("./contracts/state.json", "utf8")
-const envr = ENV === "DEV" ? "local" : "mainnet"
+
+const w = JSON.parse(fs.readFileSync("./wallet.json", "utf8"))
+const src = fs.readFileSync("./swiftify.js", "utf8")
+const istate = fs.readFileSync("./state.json", "utf8")
+
 
 console.log("🚀 Contract deployment started ⏰ ⏰.....")
 
 const contract = await createContract({
     wallet: w,
     contractSource: src,
-    environment: "testnet",
+    environment: "mainnet",
     initialState: istate,
 });
 
-fs.writeFileSync("./contracts/deployment.json", JSON.stringify({ contractAddr: contract.contractTxId, network: "testnet" }), "utf8")
+fs.writeFileSync("./deployment.json", JSON.stringify({ contractAddr: contract.contractTxId, network: "testnet" }), "utf8")
 console.log("🚀 Contract deployed ✅ ✅.....", contract.contractTxId)
